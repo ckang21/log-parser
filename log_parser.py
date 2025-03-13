b53ec15f276bc3cd5b65cc438e4ec4348de0c77a
Christian Kang <christiankang@christians-air.lan>
2025-03-12 21:59:11 -0600

    Testing errors and filtering just for those

--- log_parser.py
+++ log_parser.py
@@ -3,9 +3,15 @@ import click
 @click.command()
 @click.argument("logfile", type=click.File("r"))
 def read_log(logfile):
-    """Reads log and print all lines."""
+    """Reads log and print only ERRORS."""
+    total_errors = 0
     for line in logfile:
-        print(line.strip())
+        line = line.strip() # Clean it first
+        if "ERROR" in line:
+            print(line)
+            total_errors += 1 # Increment if we find one
+
+    print(f"\nTotal Errors is: {total_errors}")
 
 if __name__ == "__main__":
     read_log()
